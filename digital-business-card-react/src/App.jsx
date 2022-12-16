f1950427dac876f1d778ec29405e7b1e52f43056
ChineduEjiogu1 <chineduejiogu@gmail.com>
2022-12-16 22:19:12 +0000

    First draft

---
+++ digital-business-card-react/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css'
+import MainCard from './components/MainCard'
+
+export default function App()
+{
+  return (
+    <div className='main-container'>
+      <MainCard />
+    </div>
+  )
+}
---
+++ digital-business-card-react/src/components/MainCard.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './MainCard.css'
+import picOfMe from '/src/assets/picture_of_me.jpg'
+import instagram from '/src/assets/instagram.svg'
+import linkedin from '/src/assets/linkedin.svg'
+import github from '/src/assets/github.svg'
+import mail from '/src/assets/mail.svg'
+import twitter from '/src/assets/twitter.svg'
+
+export default function MainCard(){
+    return (
+        <>
+            <div className='container'>
+                <div className='img-container'>
+                <img className='main-img' src={picOfMe} alt="Picture of me" />
+                </div>
+                <div className='sub-container'>
+                    <h2 className='name'>Chinedu Ejiogu</h2>
+                    <h4 className='role'>Full-Stack Developer</h4>
+                    <h5 className="know-more">
+                        <a href="https://chineduejiogu1.github.io/portfolio_website_tkh/" target="_" rel="noopener noreferrer">
+                            Know more
+                        </a> 
+                    </h5>
+                    <div className='connect'>
+                        <a className='mail' href="mailto:chineduejiogu@gmail.com"><img src={mail} alt="mail" /> Email</a>
+                        <a className='linkedin' href="https://www.linkedin.com/in/chinedu-ejiogu/" target='_blank'><img src={linkedin} alt="" />LinkedIn</a>
+                    </div>
+                    <div className='about'>
+                        <h3>About</h3>
+                        <p>I am an aspiring full-stack web developer. My joy for Computer Science and Web Development comes from my joy of playing legos as a kid, also I'm infatuated with creating, making things, and problem-solving. </p>
+                    </div>
+                    <div className='interest'>
+                        <h3>Interests</h3>
+                        <p>What I do with my spare time is reading, play video games, writing, cooking, and going for long walks.</p>
+                    </div>
+                </div>
+                <div className='footer'>
+                    <a href="https://github.com/ChineduEjiogu1"><img className='github' src={github} alt="github" /></a>
+                    <a href="https://www.instagram.com/chinedu_eric/"><img className='instagram' src={instagram} alt="instagram" /></a>
+                    <a href="https://twitter.com/Chinedu_Ejiogu"><img className='twitter' src={twitter} alt="twitter" /></a>
+                </div>
+            </div>
+        </>
+    )
+}
